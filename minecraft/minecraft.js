7f00d259708669b5e9d4847438a8ae5b56c92a6a
Matthew Johnson (MSRC) <matjoh@microsoft.com>
2018-04-01 16:59:08 +0100

    Adding minecraft site

---
+++ minecraft/minecraft.js
@@ -0,0 +1,17 @@
+function setCarouselHeight(id) {
+    var slideHeight = [];
+    $(id + ' .carousel-item').each(function () {
+        // add all slide heights to an array
+        slideHeight.push($(this).height());
+    });
+
+    // find the tallest item
+    max = Math.max.apply(null, slideHeight);
+
+    // set the slide's height
+    $(id + ' .carousel-content').each(function () {
+        $(this).css('height', max + 'px');
+    });
+}
+
+setCarouselHeight('#slideshow');
